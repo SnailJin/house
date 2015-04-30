c7d88f46e4257778092fe9d69ea29d01ceaeebfe
SnailJin <jin521436@163.com>
2015-04-30 17:44:30 +0800

    debug

--- common/excel.py
+++ common/excel.py
@@ -11,7 +11,7 @@ import datetime
 from common.commit import get_obj_attr
 from apps.agent.models import RecommendRecord
 import os
-from house.settings import PATH
+from house.settings import PATH, MEDIA_ROOT
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -20,7 +20,7 @@ def create_excel():
     time=now.strftime('%Y-%m-%d')
     date_from = datetime.datetime(now.year, now.month, now.day-1, 0, 0, 0)
     date_to = datetime.datetime(now.year, now.month, now.day, 0, 0, 0)
-    filePath=os.path.join(PATH,'xslx/%s.xlsx'%(time)).replace('\\','/')
+    filePath=os.path.join(os.path.dirname(MEDIA_ROOT),'xslx/%s.xlsx'%(time)).replace('\\','/')
     workbook = xlsxwriter.Workbook(filePath)
     #推荐人
     headersList=['姓名','性别','身份证号','联系方式','银行卡卡号','注册时间']
